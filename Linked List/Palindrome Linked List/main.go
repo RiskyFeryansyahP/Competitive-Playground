bb78de3de140de9496389557dceff6c187ac2e6e
Risky Feryansyah Pribadi <riskypribadi24@gmail.com>
2020-09-28 01:01:41 +0700

    add solved palindrome linked list

---
+++ Linked List/Palindrome Linked List/main.go
@@ -0,0 +1,60 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, World!")
+}
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func isPalindrome(head *ListNode) bool {
+	slow := head
+	fast := head
+
+	// 1->2->2->1
+	for fast != nil {
+		if fast.Next == nil {
+			break
+		}
+
+		fast = fast.Next.Next
+		slow = slow.Next
+	}
+
+	// 1->2->2->nil, because the slow doing reverse the linked list
+	fast = head
+	slow = reverseLinkedList(slow)
+
+	// slow after reverse linkedlist 1->2->nil (simplify: 1->2->2->nil<-2<-1)
+	for slow != nil {
+		if slow.Val != fast.Val {
+			return false
+		}
+
+		slow = slow.Next
+		fast = fast.Next
+	}
+
+	return true
+}
+
+func reverseLinkedList(node *ListNode) *ListNode {
+	currentNode := node
+	var prev *ListNode
+	var next *ListNode
+
+	for currentNode != nil {
+		next = currentNode.Next
+		currentNode.Next = prev
+		prev = currentNode
+		currentNode = next
+	}
+
+	return prev
+}
